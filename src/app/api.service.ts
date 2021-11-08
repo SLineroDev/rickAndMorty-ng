442e8ba2292ad238142c364dce39e1cc45e5a10b
Sergio Linero <SLineroDev@gmail.com>
2021-11-08 20:52:31 +0100

    Fixwewd Assets

--- src/app/api.service.ts
+++ src/app/api.service.ts
@@ -10,7 +10,7 @@ export class ApiService {
   constructor(private http: HttpClient) { }
 
   getJsonData(): Observable<CharacterResp> {
-    return this.http.get<CharacterResp>('/assets/data.json')
+    return this.http.get<CharacterResp>('assets/data.json')
   }
 
   getApiData(): Observable<CharacterResp> {
